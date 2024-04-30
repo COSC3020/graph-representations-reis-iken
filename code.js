ff07dbf70edee6398f289bd452926889b3683245
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-04-30 12:47:14 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    let adjList = [];
+    let vertices = adjMatrix.length;
+    for (let i = 0; i < vertices; i++) {
+        let surroundings = [];
+        for (let j = 0; j < vertices; j++) {
+            if (adjMatrix[i][j] === 1) {
+                surroundings.push(j);
+            }
+        }
+        adjList.push(surroundings);
+    }
+    return adjList;
 }
